ab2435f2c954ec0d273ae703982909eeb162e684
wwdd23 <woody@siqitao.com>
2021-08-15 23:22:41 +0800

    *update) 添加显示器切换脚本

---
+++ mac_screen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###################
+# Author:  wudi
+# Mail: programmerwudi@gmail.com 
+# Description: 
+# Created Time: 2021-08-15 23:22:22
+###################
+
+
+
+sudo pmset -a GPUSwitch 0
+
+
+sudo pmset -a GPUSwitch 2
